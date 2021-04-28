c4f848293b8a6fa357b2db6c584e41c6b5f9c016
soaza <65228562+soaza@users.noreply.github.com>
2021-04-28 10:20:39 +0800

    Add skip intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React, { FC } from "react";
 import "./App.css";
-import { Typography, Row, Col } from "antd";
+import { Typography } from "antd";
 import Typewriter from "typewriter-effect";
 import FadeIn from "react-fade-in";
 import Navbar from "./components/Navigation/navbar";
 import AboutMe from "./components/AboutMe/aboutme";
 import Project from "./components/MyProjects/projects";
 import TechStack from "./components/TechStack/tech";
-
-const { Title } = Typography;
+const { Title, Link } = Typography;
 const { useState } = React;
 
 const App: FC = () => {
   const [showTypewriter, setShowTypewriter] = useState(true);
+  const [textColor, setTextColor] = useState("black");
   return (
     <>
       <html>
@@ -29,33 +29,53 @@ const App: FC = () => {
           </FadeIn>
         )}
         {showTypewriter && (
-          <Title
-            style={{
-              fontSize: "100px",
-              fontFamily: "Roboto Slab",
-              textAlign: "center",
-              marginTop: "20%",
-            }}
-          >
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter
-                  .changeDelay(35)
-                  .typeString("Hi, I am Kim Guan")
-                  .pauseFor(1000)
-                  .deleteAll()
-                  .typeString("Welcome to my website.")
-                  .pauseFor(1000)
-                  .deleteAll()
-                  .typeString("Lets get started,shall we?")
-                  .pauseFor(1000)
-                  .deleteAll()
-                  .callFunction(() => setShowTypewriter(false))
-
-                  .start();
+          <>
+            <div></div>
+            <Title
+              style={{
+                fontSize: "100px",
+                fontFamily: "Roboto Slab",
+                textAlign: "center",
+                marginTop: "20%",
               }}
-            />
-          </Title>
+            >
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .changeDelay(35)
+                    .typeString("Hi, I am Kim Guan")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString("Welcome to my website.")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString("Lets get started,shall we?")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .callFunction(() => setShowTypewriter(false))
+
+                    .start();
+                }}
+              />
+            </Title>
+            <Link
+              onMouseEnter={() => setTextColor("grey")}
+              onMouseLeave={() => setTextColor("black")}
+            >
+              <Title
+                onClick={() => setShowTypewriter(false)}
+                style={{
+                  opacity: 0.8,
+                  color: textColor,
+                  fontSize: "24px",
+                  fontFamily: "Roboto Slab",
+                  textAlign: "center",
+                }}
+              >
+                Skip intro{" "}
+              </Title>
+            </Link>
+          </>
         )}
       </html>
     </>
